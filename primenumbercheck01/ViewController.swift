46bdd7680a9a5c177cc66ec05cd42a7d7886affd
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-03-29 16:49:13 +0900

    ha

--- primenumbercheck01/ViewController.swift
+++ primenumbercheck01/ViewController.swift
@@ -1,25 +1,60 @@
 //
 //  ViewController.swift
-//  primenumbercheck01
+//  PrimeNumber
 //
-//  Created by D7703_28 on 2018. 3. 29..
-//  Copyright © 2018년 YDuChoi. All rights reserved.
+//  Created by D7703_17 on 2018. 3. 29..
+//  Copyright © 2018년 D7703_17. All rights reserved.
 //
-
 import UIKit
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate{
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var txtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btNumberCheck(_ sender: UIButton) {
+        let Number=Int(txtField.text!)
+        var Prime = true
+        if Number != 1 && Number != 2 {
+            for i in 2 ..< Number! {
+                if Number! % i == 0{
+                    Prime = false
+                }
+            }
+            if Prime == true {
+                label.text="Prime Number!!"
+            } else {
+                label.text="Not Prime number!!"
+            }
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+    }
+    
+    @IBAction func btNumberReset(_ sender: UIButton) {
+        txtField.text=""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtField.resignFirstResponder()
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField)
+        -> Bool {
+            txtField.resignFirstResponder()
+            return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        label.resignFirstResponder()
+        return true
+        
+    }
 }
-
